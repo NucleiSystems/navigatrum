f56b6e66a6d448fbb4f39dff1bf4b4b8f5c1c545
wizock <silent.death3500@gmail.com>
2023-06-29 21:58:46 +1000

    .

--- src/auth/token_handler.tsx
+++ src/auth/token_handler.tsx
@@ -32,19 +32,16 @@ export const LoggedInChecker = ({ children: child }: any) => {
 };
 
 export const LoginInspector = async () => {
-  let token = fetchToken()
-  const tokenChecker = await axios.post(
-    "https://single-orca-f1izhs.ziska44n.traefikhub.io/users/token/check",
-    {
+  let token = fetchToken();
+  const tokenChecker = await axios
+    .post("https://nucleibackend.systems/users/token/check", {
       headers: {
         accept: "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-      token: `${localStorage.getItem("token")}`
-    }
-  ).then(
-    (response=>{
-      console.log(response.data)
+      token: `${localStorage.getItem("token")}`,
     })
-  )
+    .then((response) => {
+      console.log(response.data);
+    });
 };
